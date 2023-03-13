aa513ae420080c8d017ec601a06c39a6f87c57e0
driano1221 <117847780+driano1221@users.noreply.github.com>
2023-03-13 15:35:42 -0300

    Trabalho
    Mudado dia 13/03

---
+++ docs/Index.R
@@ -0,0 +1,8 @@
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("knitr")
+library(rmarkdown)
+require(xaringan)
+Anonymous> -> normalize_path
+render("Index.Rmd..Rmd", output_format = "pdf_document")
+pandoc()
